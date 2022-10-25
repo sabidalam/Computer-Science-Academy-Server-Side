47fa76a966e997c2d2faea227a236f0bb3a8722f
sabidalam <Shafkatsaad@gmail.com>
2022-10-25 19:50:30 +0600

    loaded-courses-data

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ const port = process.env.PORT || 5000;
 
 const topics = require('./data/topics.json');
 
-
+const courses = require('./data/courses.json');
 
 app.use(cors());
 
 app.get('/topics', (req, res) => {
     res.send(topics);
 })
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+
 
 
 app.listen(port, () => {
