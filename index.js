345771352068b82befc394feca6c471352a660b2
sabidalam <Shafkatsaad@gmail.com>
2022-10-25 19:59:46 +0600

    create-dynamic-side-for-every-course

--- index.js
+++ index.js
@@ -23,7 +23,11 @@ app.get('/courses', (req, res) => {
     res.send(courses);
 })
 
-
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(course => course._id === id)
+    res.send(selectedCourse);
+})
 
 app.listen(port, () => {
     console.log('Computer Science', port);
