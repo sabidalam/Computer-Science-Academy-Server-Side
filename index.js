c8c2b2087bbab15f44425fd820a6fcadeab1d82f
sabidalam <Shafkatsaad@gmail.com>
2022-10-25 19:46:10 +0600

    load-topic-data

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const topics = require('./data/topics.json');
+
+
+
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('')
+app.get('/topics', (req, res) => {
+    res.send(topics);
 })
+
 
 app.listen(port, () => {
     console.log('Computer Science', port);
